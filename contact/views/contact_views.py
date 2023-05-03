c74fc894fde26ab1ae6df1f186d81c974dfd2479
Pythonauts <pythonauts@gmail.com>
2023-05-02 22:16:57 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -21,9 +21,12 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(id=contact_id).first()
     single_contact = get_object_or_404(Contact, id=contact_id, show=True)
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} -'
+
     context = {
         'contact': single_contact,
-    }
+        'site_title': contact_name,
+        }
 
     return render(
         request,
