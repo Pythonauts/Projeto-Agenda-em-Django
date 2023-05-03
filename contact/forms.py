33f9f17a26933406d01b5caf4bfecc4321fee508
Pythonauts <pythonauts@gmail.com>
2023-05-03 17:27:37 -0300

    Validando campos de first_name, last_name e email do Usuário

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -13,9 +14,7 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = models.Contact
-        fields = (
-            'first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture',
-        )
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture',)
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -33,16 +32,28 @@ class ContactForm(forms.ModelForm):
         first_name = self.cleaned_data.get('first_name')
 
         if first_name == 'ABC':
-            self.add_error(
-                'first_name',
-                ValidationError(
-                    'Veio do add_error',
-                    code='invalid'
-                )
-            )
+            self.add_error('first_name',
+                           ValidationError('Veio do add_error', code='invalid')
+                           )
 
         return first_name
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(required=True, min_length=3,)
+
+    last_name = forms.CharField(required=True, min_length=3,)
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password1', 'password2',
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', ValidationError('Já existe este e-mail', code='invalid'))
+
+        return email
