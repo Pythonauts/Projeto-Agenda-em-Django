518465d3a62be85392967b6b47827f0ba592c641
Pythonauts <pythonauts@gmail.com>
2023-05-03 10:21:56 -0300

    Usando a classe Paginator para paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.http import Http404
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=(True)).order_by('-id')[0:10]
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Contatos -'
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -25,16 +31,24 @@ def search(request):
     if search_value == '':
         return redirect('contact:index')
 
-    contacts = Contact.objects.filter(show=(True))\
-        .filter(Q(first_name__icontains=search_value) |
-                Q(last_name__icontains=search_value) |
-                Q(phone__icontains=search_value) |
-                Q(email__icontains=search_value))\
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Search -'
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -45,14 +59,15 @@ def search(request):
 
 
 def contact(request, contact_id):
-    # single_contact = Contact.objects.filter(id=contact_id).first()
-    single_contact = get_object_or_404(Contact, id=contact_id, show=True)
-
-    contact_name = f'{single_contact.first_name} {single_contact.last_name} -'
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
-        'site_title': contact_name,
+        'site_title': site_title
     }
 
     return render(
